36842ad79f80af0a72876daaf05adaf7d83eefe9
swan.blanc <swan.blanc@infomaniak.com>
2020-05-05 17:50:48 +0200

    add fine tuning

--- cnn_model4.py
+++ cnn_model4.py
@@ -9,14 +9,15 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import classification_report
-from tensorflow.keras.layers import Dense, Flatten, Dropout, BatchNormalization
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras.optimizers import SGD
-
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
 from creat_train_dataset import ImageCreatTrainDataset
 from files_tools import save_json_file
-from alexnet import AlexNet
 
 PYTHON_LOGGER = logging.getLogger(__name__)
 if not os.path.exists("log"):
@@ -34,16 +35,10 @@ PYTHON_LOGGER.setLevel(logging.DEBUG)
 # Absolute path to the folder location of this python file
 FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
 DATASET = os.path.join(FOLDER_ABSOLUTE_PATH, "dog_cat_dataset")
-IMG_DIM = 128
-EPOCHS = 20
+IMG_DIM = 224
+EPOCHS = 50
 BATCH_SIZE = 64
-LEARNING_RATE = 0.01
-LAYER_DIM = IMG_DIM * IMG_DIM * 3
-LAYERS = [
-    LAYER_DIM * 2,
-    LAYER_DIM,
-    LAYER_DIM // 2
-]
+LEARNING_RATE = 0.001
 
 # Dataset
 dataset = ImageCreatTrainDataset(DATASET, IMG_DIM)
@@ -52,11 +47,24 @@ train_x, train_y = dataset.get_train_data()
 test_x, test_y = dataset.get_test_data()
 labels, nb_labels = dataset.get_labels()
 
-model = AlexNet.build(width=IMG_DIM, height=IMG_DIM, depth=3, classes=2, reg=0.0002)
-
 loss = "categorical_crossentropy" if nb_labels > 2 else "binary_crossentropy"
 
-sgd = SGD(lr=LEARNING_RATE, decay=LEARNING_RATE / EPOCHS, momentum=0.9)
+baseModel = VGG16(weights="imagenet", include_top=False, input_tensor=Input(shape=(IMG_DIM, IMG_DIM, 3)))
+
+for layer in baseModel.layers:
+    layer.trainable = False
+
+headModel = baseModel.output
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(256, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+
+# add a softmax layer
+headModel = Dense(nb_labels, activation="softmax")(headModel)
+
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+sgd = SGD(lr=LEARNING_RATE)
 model.compile(loss=loss, optimizer=sgd, metrics=["accuracy"])
 H = model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=EPOCHS, batch_size=BATCH_SIZE)
 
@@ -65,20 +73,6 @@ H = model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=EPOCHS,
 PYTHON_LOGGER.info("Evaluating network")
 predictions = model.predict(test_x, batch_size=BATCH_SIZE)
 print(classification_report(test_y.argmax(axis=1), predictions.argmax(axis=1), target_names=labels))
-
-# plot the training loss and accuracy
-range_plot = np.arange(0, EPOCHS)
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(range_plot, H.history["loss"], label="train_loss", color='red')
-plt.plot(range_plot, H.history["val_loss"], label="val_loss", color='green')
-plt.plot(range_plot, H.history["accuracy"], label="train_acc", color='blue')
-plt.plot(range_plot, H.history["val_accuracy"], label="val_acc", color='pink')
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend()
-plt.show()
 
 save_json_file({"img_dim": IMG_DIM, "labels": labels}, "model_4.json")
 model.save("model_4.h5")
